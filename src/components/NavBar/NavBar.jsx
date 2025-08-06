047b71f9a260251350d2c72b95f6b573576ef624
“AM-973” <AlMatar973@gmail.com>
2025-08-07 00:04:03 +0300

    final

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,27 @@
-// src/components/NavBar/NavBar.jsx
+import { Link } from 'react-router-dom'
+import { AuthedUserContext } from '../../App'
+import { useContext } from 'react'
 
-const NavBar = () => {
+const NavBar = (props) => {
+
+  const user = useContext(AuthedUserContext)
+
   return (
     <nav>
-      <p>Navbar contents will go here.</p>
+      {user ? (
+        <ul>
+          <li>Welcome {user.username}</li>
+          <li><Link to="/"> Home </Link></li>
+          <li><Link to='/' onClick={props.handleSignOut}>Sign Out</Link></li>
+        </ul>
+        ) : (
+          <ul>
+            <li><Link to="/sign-up">Sign Up</Link></li>
+            <li><Link to="/sign-in">Sign In</Link></li>
+          </ul>
+          ) }
     </nav>
-  );
-};
+  )
+}
 
-export default NavBar;
-
+export default NavBar 
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+const SignIn = (props) => {
+
+  useEffect(() => {
+    if (props.user) {
+      navigate('/')
+    }
+  }, [])
+
+  const navigate = useNavigate()
+  const initialState = {
+    username: '',
+    password: '',
+  }
+
+  const [formData, setFormData] = useState(initialState)
+
+  const handleChange = (evt) => {
+    setFormData({...formData, [evt.target.name]: evt.target.value})
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    props.handleSignIn(formData)
+    navigate('/')
+  }
+
+  return (
+    <main>
+      <h1>Sign In Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <input type="text" name='username' onChange={handleChange} />
+        <br />
+        <label>Password:</label>
+        <input type="password" name='password' onChange={handleChange} />
+        <br />
+        <button type="submit">Sign In</button>
+      </form>
+    </main>
+  )
+}
+
+export default SignIn
